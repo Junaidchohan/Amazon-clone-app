5733f2980d674bc3e5b4b08b5602a4e302b5b003
Junaidchohane <junaidchohanedu@gmail.com>
2025-07-16 14:42:47 +0500

    Picking Images

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:amazon_clone_app/common/widgets/custom_button.dart';
 import 'package:amazon_clone_app/common/widgets/custome_textfield.dart';
 import 'package:amazon_clone_app/constants/global_variables.dart';
+import 'package:amazon_clone_app/constants/utils.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +23,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController quantityController = TextEditingController();
 
   String category = 'Mobiles';
+  List<File> images = [];
 
   @override
   void dispose() {
@@ -36,6 +41,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,36 +70,57 @@ class _AddProductScreenState extends State<AddProductScreen> {
             child: Column(
               children: [
                 SizedBox(height: 20),
-                DottedBorder(
-                  options: RectDottedBorderOptions(
-                    dashPattern: [10, 5],
-                    strokeWidth: 2,
-                    padding: EdgeInsets.all(10),
-                  ),
+                images.isNotEmpty
+                    ? CarouselSlider(
+                      items:
+                          images.map((i) {
+                            return Builder(
+                              builder:
+                                  (BuildContext context) => Image.file(
+                                    i,
+                                    fit: BoxFit.cover,
+                                    height: 200,
+                                  ),
+                            );
+                          }).toList(),
+                      options: CarouselOptions(
+                        viewportFraction: 1,
+                        height: 200,
+                      ),
+                    )
+                    : GestureDetector(
+                      onTap: selectImages,
+                      child: DottedBorder(
+                        options: RectDottedBorderOptions(
+                          dashPattern: [10, 5],
+                          strokeWidth: 2,
+                          padding: EdgeInsets.all(10),
+                        ),
 
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.folder_open, size: 40),
-                        SizedBox(height: 15),
-                        Text(
-                          "Select Product Image",
-                          style: TextStyle(
-                            color: Colors.grey.shade400,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                        child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.folder_open, size: 40),
+                              SizedBox(height: 15),
+                              Text(
+                                "Select Product Image",
+                                style: TextStyle(
+                                  color: Colors.grey.shade400,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
                 SizedBox(height: 30),
                 CustomeTextfield(
                   controller: productNameController,
