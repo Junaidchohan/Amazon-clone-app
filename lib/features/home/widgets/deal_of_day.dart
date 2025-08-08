1041f7e22f3878224b96edff1ea52efa6517884b
Junaidchohane <junaidchohanedu@gmail.com>
2025-08-08 23:40:54 +0500

    Fetch Deal of The Day

--- lib/features/home/widgets/deal_of_day.dart
+++ lib/features/home/widgets/deal_of_day.dart
@@ -1,3 +1,7 @@
+import 'package:amazon_clone_app/common/widgets/loader.dart';
+import 'package:amazon_clone_app/features/home/sevices/home_services.dart';
+import 'package:amazon_clone_app/features/product_details/screens/product_details_screen.dart';
+import 'package:amazon_clone_app/models/product.dart';
 import 'package:flutter/material.dart';
 
 class DealOfDay extends StatefulWidget {
@@ -8,72 +12,90 @@ class DealOfDay extends StatefulWidget {
 }
 
 class _DealOfDayState extends State<DealOfDay> {
+  Product? product;
+  final HomeServices homeServices = HomeServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDealOfDay();
+  }
+
+  void fetchDealOfDay() async {
+    product = await homeServices.fetchDealOfDay(context: context);
+    setState(() {});
+  }
+
+  void navigateToDetailScreen() {
+    Navigator.pushNamed(
+      context,
+      ProductDetailsScreen.routName,
+      arguments: product,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(left: 10, top: 15),
-          child: Text("Deal of the day", style: TextStyle(fontSize: 20)),
-        ),
-        Image.network(
-          "https://www.shutterstock.com/shutterstock/photos/2591636917/display_1500/stock-photo-kiev-ukraine-february-apple-iphone-pro-black-titanium-2591636917.jpg",
-          height: 235,
-          fit: BoxFit.fitHeight,
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-
-          padding: EdgeInsets.only(left: 15, top: 5, right: 40),
-          child: Text("\$100", style: TextStyle(fontSize: 18)),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(left: 15, top: 5, right: 40),
-          child: Text("Junaid", maxLines: 2, overflow: TextOverflow.ellipsis),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return product == null
+        ? Loader()
+        : product!.name.isEmpty
+        ? SizedBox()
+        : GestureDetector(
+          onTap: navigateToDetailScreen,
+          child: Column(
             children: [
-              Image.network(
-                "https://www.shutterstock.com/shutterstock/photos/2502458641/display_1500/stock-photo-the-box-with-the-iphone-is-on-a-laptop-on-the-screen-of-which-there-is-a-render-with-the-iphone-2502458641.jpg",
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 10, top: 15),
+                child: Text("Deal of the day", style: TextStyle(fontSize: 20)),
               ),
               Image.network(
-                "https://www.shutterstock.com/shutterstock/photos/2502458641/display_1500/stock-photo-the-box-with-the-iphone-is-on-a-laptop-on-the-screen-of-which-there-is-a-render-with-the-iphone-2502458641.jpg",
+                product!.images[0],
+                height: 235,
                 fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
               ),
-              Image.network(
-                "https://www.shutterstock.com/shutterstock/photos/2502458641/display_1500/stock-photo-the-box-with-the-iphone-is-on-a-laptop-on-the-screen-of-which-there-is-a-render-with-the-iphone-2502458641.jpg",
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
+              Container(
+                alignment: Alignment.topLeft,
+
+                padding: EdgeInsets.only(left: 15, top: 5, right: 40),
+                child: Text("\$100", style: TextStyle(fontSize: 18)),
               ),
-              Image.network(
-                "https://www.shutterstock.com/shutterstock/photos/2502458641/display_1500/stock-photo-the-box-with-the-iphone-is-on-a-laptop-on-the-screen-of-which-there-is-a-render-with-the-iphone-2502458641.jpg",
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 15, top: 5, right: 40),
+                child: Text(
+                  "Junaid",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:
+                      product!.images
+                          .map(
+                            (e) => Image.network(
+                              e,
+                              fit: BoxFit.fitHeight,
+                              width: 100,
+                              height: 100,
+                            ),
+                          )
+                          .toList(),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 15).copyWith(left: 15),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "See all deals",
+                  style: TextStyle(color: Colors.cyan[800]),
+                ),
               ),
             ],
           ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 15).copyWith(left: 15),
-          alignment: Alignment.topLeft,
-          child: Text(
-            "See all deals",
-            style: TextStyle(color: Colors.cyan[800]),
-          ),
-        ),
-      ],
-    );
+        );
   }
 }
